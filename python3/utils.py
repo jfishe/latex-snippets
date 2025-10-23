d5ca42b138998897419ef6780142ff8d71c680e7
John D. Fisher <jdfenw@gmail.com>
2025-10-23 08:20:10 -0700

    fix(utils): add missing file

---
+++ python3/utils.py
@@ -0,0 +1,11 @@
+import vim
+
+def math():
+  return vim.eval('vimtex#syntax#in_mathzone()') == '1'
+
+def comment():
+  return vim.eval('vimtex#syntax#in_comment()') == '1'
+
+def env(name):
+  [x,y] = vim.eval("vimtex#env#is_inside('" + name + "')")
+  return x != '0' and y != '0'
